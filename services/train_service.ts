bbf103fae678964a8c75d777fd7cafc48d2e2b86
Theis Jensen <53541469+Theis194@users.noreply.github.com>
2026-03-30 09:02:10 +0200

    remove prints

--- services/train_service.ts
+++ services/train_service.ts
@@ -8,8 +8,6 @@ export default async function post_train(req: Request) {
     data[key] = value;
   }
 
-  console.log(data);
-
   const targetUrl = new URL(`http://127.0.0.1:5002/train/${data.model_name}`);
 
   const response = await fetch(targetUrl.toString(), {
@@ -20,11 +18,8 @@ export default async function post_train(req: Request) {
     body: JSON.stringify(data),
   });
 
-  //console.log(await response.json());
-
   const json = await response.json();
   const job_id = json.job_id;
-  console.log(job_id);
 
   return RedirectResponse(`/running-jobs?job_id=${job_id}`);
 }
