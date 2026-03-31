4a3e460ba720c7852e2780122cf9a53c9ffc6c7d
AndreasBM1 <113091983+AndreasBM1@users.noreply.github.com>
2026-03-31 12:05:43 +0200

    required not always required

--- components/render_parameter_form.tsx
+++ components/render_parameter_form.tsx
@@ -86,7 +86,7 @@ function generate_fieldset(parameter: Parameter) {
             type="number"
             name={parameter_name}
             class="input validator w-full"
-            required
+            required={parameter.name !== "seed" || undefined}
             min={parameter.min}
             max={parameter.max}
             value={parameter.default}
