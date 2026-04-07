0387f8b05f60cce90ef3ebf6b60f0e2aefc89c90
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2026-04-07 11:00:16 +0200

    cat fix

--- pages/train.tsx
+++ pages/train.tsx
@@ -1,5 +1,6 @@
 import { render_default_page } from "../components/default_templates.tsx";
 import render_parameter_form from "../components/render_parameter_form.tsx";
+import { errorCodeCatImage } from "../components/responses.tsx";
 import { getValue, hasValue } from "../optional.ts";
 import { get_model_names, get_parameters } from "../services/hgnn_service.ts";
 
@@ -8,7 +9,13 @@ export async function train_a_model_page(_req: Request) {
   const model_names = await get_model_names();
 
   if (!hasValue(parameters) || !hasValue(model_names)) {
-    return <>"Python Server"</>;
+     return render_default_page(
+          `No connection to the Python server!`,
+          <>
+            No connection to the Python server!
+            {errorCodeCatImage(521)}
+          </>,
+        );
   }
   return render_default_page(
     "Welcome",
