aefb10bcb760a7140d5df98d3b6f56e49b7d69b2
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2026-04-23 12:00:53 +0200

    Update train.ts

--- shared/train.ts
+++ shared/train.ts
@@ -39,7 +39,7 @@ export const Job = z.object({
   duration: z.coerce.number(),
   patience: z.coerce.number(),
   created_at: z.date(),
-  quality_weight: z.coerce.number()
+  quality_weight: z.coerce.number(),
 });
 
 export type Job = z.infer<typeof Job>;
